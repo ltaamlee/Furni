e60ea66ab3eed382c30d3d6114e768354249f1f2
ltaamlee <letam41225@gmail.com>
2026-05-10 10:06:05 +0700

    chore: fix server port format

--- src/server.js
+++ src/server.js
@@ -65,7 +65,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/furni-eco
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
